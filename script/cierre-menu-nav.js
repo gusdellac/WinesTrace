cb89eb5a28119f7ee3968c224473d6d48d91e7e9
Gustavo DeLlac <gustavodellac@gmail.com>
2022-11-29 18:51:16 -0300

    corrijo bg indicadores prev next banner, agrego section contacto, agrego boxshadow footer, modifico menu nav pagina qrgenerado y trazabotella

--- script/cierre-menu-nav.js
+++ script/cierre-menu-nav.js
@@ -1,5 +1,7 @@
 const itemServicios = document.querySelector("#menuNav-servicios");
 const itemSobreNosotros = document.querySelector("#menuNav-sobre-nosotros");
+const itemContacto = document.querySelector("#menuNav-contacto");
+
 const botonMenuNav = document.querySelector("#btn-menu");
 
 itemServicios.addEventListener("click", () => {
@@ -9,6 +11,11 @@ itemServicios.addEventListener("click", () => {
 itemSobreNosotros.addEventListener("click", () => {
     botonMenuNav.checked = false;
 });
+
+itemContacto.addEventListener("click", () => {
+    botonMenuNav.checked = false;
+});
+
 
 
 
