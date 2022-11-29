f791e535ce6aab88e805a5a161dac2a2b49c4b06
Gustavo DeLlac <gustavodellac@gmail.com>
2022-11-28 23:49:58 -0300

    corrijo cierre menu navegacion al clickear en servicios o en sobre nosotros

---
+++ script/cierre-menu-nav.js
@@ -0,0 +1,18 @@
+const itemServicios = document.querySelector("#menuNav-servicios");
+const itemSobreNosotros = document.querySelector("#menuNav-sobre-nosotros");
+const botonMenuNav = document.querySelector("#btn-menu");
+
+itemServicios.addEventListener("click", () => {
+    botonMenuNav.checked = false;
+});
+
+itemSobreNosotros.addEventListener("click", () => {
+    botonMenuNav.checked = false;
+});
+
+
+
+
+
+
+
